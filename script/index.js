78b67ce171e5c63f82c7cd720d2b40a81c17ca7c
Rajars0488 <rajarshi6666@gmail.com>
2023-02-06 23:30:48 +0530

    first commit

---
+++ script/index.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".typing", {
+    strings: [ "",
+      " Web Devloper",
+      " Web Designer",
+      " Frontend Developer.",
+    ],
+    typeSpeed: 150,
+    backSpeed: 80,
+    loop: true,
+  });
